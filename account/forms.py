addb9d82315d217ceee8c071cc8548c3a5e11e7f
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2016-02-06 02:22:31 +0000

    UI improved. Added login form

--- account/forms.py
+++ account/forms.py
@@ -2,5 +2,19 @@ from django import forms
 
 
 class VoterLoginForm(forms.Form):
-    username = forms.CharField(max_length=64, min_length=1)
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(
+        max_length=64,
+        min_length=1,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'username',
+            }
+        ))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '********',
+            }
+        ))
