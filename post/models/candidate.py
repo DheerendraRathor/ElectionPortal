4493b02ba7bd9c1a85ed3b67319e164159f424ad
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2016-02-14 22:53:42 +0530

    Increased file length to 512

--- post/models/candidate.py
+++ post/models/candidate.py
@@ -9,7 +9,7 @@ def candidate_image_upload(candidate, filename):
 
 class Candidate(models.Model):
     name = models.CharField(max_length=64, db_index=True)
-    image = models.ImageField(upload_to=candidate_image_upload, null=True, blank=True)
+    image = models.ImageField(upload_to=candidate_image_upload, max_length=512, null=True, blank=True)
     post = models.ForeignKey(Post, related_name='candidates', db_index=True)
     order = models.IntegerField(default=0, help_text='Sorting order of candidates')
     is_nota = models.BooleanField(default=False, editable=False)
